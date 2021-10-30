0bc13b33a28c490f4b1be3a2ff690847352e3a6d
ejguzman3988 <eriberto.guzman@outlook.com>
2021-10-30 05:14:49 -0400

    Adds route for EntryPage.js at '/' and creates Animation.css with spin keyframe

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
+import "../stylesheets/Animations.css";
 import EntryPage from "./EntryPage/EntryPage";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  return <div className="App"><Router>
-    <Switch>
-      <Route exact path="/" component={EntryPage} />
-    </Switch>
-    </Router></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={EntryPage} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
