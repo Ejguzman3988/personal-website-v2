3af6d0a9907765f008c32e8ddf08b9faea3fa7b9
ejguzman3988 <eriberto.guzman@outlook.com>
2021-11-01 07:11:24 -0400

    creates choices component, adds a bit of styling for choices. Adds a few variables to variables css

--- src/components/Choices/Choices.js
+++ src/components/Choices/Choices.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./Choices.css";
 
 const Choices = () => {
-  return <div></div>;
+  return (
+    <div className="choices">
+      <h1 className="choices__info">Info</h1>
+      <h1 className="choices__journey">Journey</h1>
+    </div>
+  );
 };
 
 export default Choices;
