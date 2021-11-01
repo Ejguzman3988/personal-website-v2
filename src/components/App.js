4741d723de26894a331b744cf8a753526351b366
ejguzman3988 <eriberto.guzman@outlook.com>
2021-11-01 07:12:00 -0400

    Adds NavBar component for easy navigating.

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,13 @@ import "../stylesheets/Animations.css";
 import EntryPage from "./EntryPage/EntryPage";
 import Choices from "./Choices/Choices";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavBar />
         <Switch>
           <Route exact path="/" component={EntryPage} />
           <Route exact path="/choices" component={Choices} />
