1aa5e30f9d51bcbbd53f89331fbaa8a6575e5dd4
ejguzman3988 <eriberto.guzman@outlook.com>
2021-11-02 07:51:44 -0400

    Adds new variable --darker-blue, creates tge info.css file and creates static info component with resume and projects

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./Info.css";
 
 const Info = () => {
-  return <div>INFO!</div>;
+  return (
+    <div className="info__container">
+      <div className="info__resume">Resume</div>
+      <div className="info__projects">Projects</div>
+    </div>
+  );
 };
 
 export default Info;
